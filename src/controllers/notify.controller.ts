dcebc1e636eb1cf888a8b35807bc028f2e8309ea
juan morales <juanskate326@gmail.com>
2023-07-07 09:20:27 -0500

    07/07

--- src/controllers/notify.controller.ts
+++ src/controllers/notify.controller.ts
@@ -1,23 +1,50 @@
 import configEnv from "../config";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import pool from "../database";
+import { getSocketId } from "../socketHandlers/user.handlers";
+import { getSocketInstance } from "../socket";
 
+export const readNotify = async (req: any, res: any) => {
+  const token = req.headers["x-access-token"];
 
+  try {
+    const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
 
-export const getNotification = async (req: any, res: any) => {
+    const [rows]: any[] = await pool.query(
+      "SELECT * FROM usuario_notificaciones WHERE usuario_id = ? ORDER BY time DESC",
+      [decoded.id]
+    );
 
-	const token = req.headers['x-access-token'];
+    res.json(
+      rows.map((data) => {
+        return { ...data, data: JSON.parse(data.data) };
+      })
+    );
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener las notificaciones" });
+  }
+};
+export const createNotify = async (req: any, res: any) => {
+  try {
+    const { usuario_id, title, message, data, time, type, estado } = req.body;
+    console.log("body:::", usuario_id);
+    const [results]: any = await pool.query(
+      "INSERT INTO usuario_notificaciones (usuario_id,title,message,data,time,type,estado) VALUES (?,?,?,?,?,?,?)",
+      [usuario_id, title, message, JSON.stringify(data), time, type, estado]
+    );
+    res.status(200).json(results);
 
-	try {
-		const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
-        
-        const [rows]:any[] = await pool.query("SELECT * FROM usuarioNotificaciones WHERE usuario_id = ? ORDER BY time DESC", [decoded.id]);
+    const io = getSocketInstance();
 
-        res.json(rows.map((data)=> {return{...data, data:JSON.parse(data.data)}}));
+    const socketId = await getSocketId(usuario_id);
 
+    if (socketId) io.to(socketId).emit("notify", req.body);
 
-    } catch (error) {
-        res.status(500).json({ message: "Error al obtener las notificaciones" });
-    }
+  } catch (error) {
 
-}
+    console.error("Error during registered:", error);
+
+    res.status(500).send("registered failed");
+    
+  }
+};
