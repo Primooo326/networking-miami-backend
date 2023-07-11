e2faefe7391f545204ed416f3b23ea87e661b82f
juan morales <juanskate326@gmail.com>
2023-07-11 12:14:29 -0500

    some websockets

--- src/controllers/files.controller.ts
+++ src/controllers/files.controller.ts
@@ -1,38 +1,41 @@
-import path from "path";
-import configEnv from "../config"
-import fs from "fs"
+import path from 'path';
+import configEnv from '../config';
+import fs from 'fs';
 import pool from '../database';
 import jwt from 'jsonwebtoken';
 
-
-export const uploadFile = async (req,res) =>{
+export const uploadFile = async (req, res) => {
 	const token = req.headers['x-access-token'];
-    try {
+	try {
 		const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
-        const [result]: any = await pool.query('UPDATE usuario SET avatar = ? WHERE id = ?', [`${configEnv.URL_BACK}api/file/${req.file.filename}`,decoded.id]) 
-        
-        res.json({path:`${configEnv.URL_BACK}api/file/${req.file.filename}`,request:req.file})
+		const [result]: any = await pool.query(
+			'UPDATE usuario SET avatar = ? WHERE id = ?',
+			[`${configEnv.URL_BACK}api/file/${req.file.filename}`, decoded.id],
+		);
+		console.log(result);
 
-        
-    } catch (error) {
-        res.status(500).send(error)
-    }
+		res.json({
+			path: `${configEnv.URL_BACK}api/file/${req.file.filename}`,
+			request: req.file,
+		});
+	} catch (error) {
+		res.status(500).send(error);
+	}
+};
 
-}
-
-export const getFile = async (req,res) =>{
-
-    try {
-        fs.readFile(path.join(__dirname,`../../images/${req.params.filename}`), (err,data) =>{
-            if(err) throw res.send(err);
-            res.send(data)
-        })
-        // fs.readFile(path.join(__dirname,`../images/${req.params.filename}`), (err,data) =>{
-        //     if(err) throw res.send(err);
-        //     res.send(data)
-        // })
-    } catch (error) {
-        res.send(error)
-    }
-
-}
+export const getFile = async (req, res) => {
+	try {
+		fs.readFile(
+			path.join(__dirname, `../../images/${req.params.filename}`),
+			(err, data) => {
+				if (err) {
+					console.log(err.message);
+				}
+				res.send(data);
+			},
+		);
+	} catch (error) {
+		console.log('error');
+		res.send(error);
+	}
+};
--- src/controllers/match.controller.ts
+++ src/controllers/match.controller.ts
@@ -1,297 +1,318 @@
-import pool from "../database";
-import jwt from "jsonwebtoken";
-import configEnv from "../config";
-import { getSocketId } from "../socketHandlers/user.handlers";
-import { getSocketInstance } from "../socket";
+import pool from '../database';
+import jwt from 'jsonwebtoken';
+import configEnv from '../config';
+import { getSocketId } from '../socketHandlers/user.handlers';
+import { getSocketInstance } from '../socket';
 
 export const createMatch = async (req, res) => {
-  const { idToMatch, idUser } = req.body;
-  try {
-    const [results]: any = await pool.query(
-      "SELECT * FROM usuario_contacto WHERE usuario_id = ? AND contacto_id = ?",
-      [idUser, idToMatch]
-    );
+	const { idToMatch, idUser } = req.body;
+	try {
+		const [results]: any = await pool.query(
+			'SELECT * FROM usuario_contacto WHERE usuario_id = ? AND contacto_id = ?',
+			[idUser, idToMatch],
+		);
 
-    if (results.length === 0) {
-      const [result]: any = await pool.query(
-        "INSERT INTO usuario_contacto (usuario_id,contacto_id) VALUES (?, ?)",
-        [idUser, idToMatch]
-      );
-      await pool.query(
-        "INSERT INTO usuario_contacto (usuario_id,contacto_id) VALUES (?, ?)",
-        [idToMatch, idUser]
-      );
-      await pool.query(
-        "DELETE FROM usuario_solicitudes WHERE solicitante_id = ? AND receptor_id = ?",
-        [idToMatch, idUser]
-      );
-      await pool.query(
-        "DELETE FROM usuario_notificaciones WHERE usuario_id = ?",
-        [idToMatch]
-      );
-      const io = getSocketInstance();
-      const socketId = await getSocketId(idToMatch);
+		if (results.length === 0) {
+			const [result]: any = await pool.query(
+				'INSERT INTO usuario_contacto (usuario_id,contacto_id) VALUES (?, ?)',
+				[idUser, idToMatch],
+			);
+			await pool.query(
+				'INSERT INTO usuario_contacto (usuario_id,contacto_id) VALUES (?, ?)',
+				[idToMatch, idUser],
+			);
+			await pool.query(
+				'DELETE FROM usuario_solicitudes WHERE solicitante_id = ? AND receptor_id = ?',
+				[idToMatch, idUser],
+			);
+			await pool.query(
+				'DELETE FROM usuario_notificaciones WHERE usuario_id = ?',
+				[idToMatch],
+			);
+			const socketId = await getSocketId(idToMatch);
 
-      const [userData]: any = await pool.query(
-        'SELECT * FROM usuario WHERE id = ?',
-        [idToMatch],
-      );
+			const [userData]: any = await pool.query(
+				'SELECT * FROM usuario WHERE id = ?',
+				[idToMatch],
+			);
 
-      const [lenguajesData]: any = await pool.query(
-        'SELECT lenguaje FROM usuariolenguajes WHERE usuario_id = ?',
-        [idToMatch],
-      );
+			const [lenguajesData]: any = await pool.query(
+				'SELECT lenguaje FROM usuariolenguajes WHERE usuario_id = ?',
+				[idToMatch],
+			);
 
-      const [areaExperienciaData]: any = await pool.query(
-        'SELECT experiencia FROM usuarioareaexperiencia WHERE usuario_id = ?',
-        [idToMatch],
-      );
+			const [areaExperienciaData]: any = await pool.query(
+				'SELECT experiencia FROM usuarioareaexperiencia WHERE usuario_id = ?',
+				[idToMatch],
+			);
 
-      const [temasInteresData]: any = await pool.query(
-        'SELECT interes FROM usuariotemasinteres WHERE usuario_id = ?',
-        [idToMatch],
-      );
+			const [temasInteresData]: any = await pool.query(
+				'SELECT interes FROM usuariotemasinteres WHERE usuario_id = ?',
+				[idToMatch],
+			);
 
-      const [tipoConexionData]: any = await pool.query(
-        'SELECT conexion FROM usuariotipoconexion WHERE usuario_id = ?',
-        [idToMatch],
-      );
+			const [tipoConexionData]: any = await pool.query(
+				'SELECT conexion FROM usuariotipoconexion WHERE usuario_id = ?',
+				[idToMatch],
+			);
 
-      const userDataWithRelations = {
-        ...userData[0],
-        lenguajes: lenguajesData.map((row) => row.lenguaje),
-        areaExperiencia: areaExperienciaData.map((row) => row.experiencia),
-        temasInteres: temasInteresData.map((row) => row.interes),
-        tipoConexion: tipoConexionData.map((row) => row.conexion),
-      };
+			const userDataWithRelations = {
+				...userData[0],
+				lenguajes: lenguajesData.map((row) => row.lenguaje),
+				areaExperiencia: areaExperienciaData.map((row) => row.experiencia),
+				temasInteres: temasInteresData.map((row) => row.interes),
+				tipoConexion: tipoConexionData.map((row) => row.conexion),
+			};
 
-      if (socketId) io.to(socketId).emit("newMatch", userDataWithRelations);
-      res.status(200).json({
-        result,
-      });
-    } else {
-      res.status(500).send("Match ya existe");
-    }
-  } catch (error) {
-    console.error("Error during registered:", error);
-    res.status(500).send("registered failed");
-  }
+			const ioSocket = getSocketInstance();
+
+			if (socketId) {
+				ioSocket.to(socketId).emit('newMatch', userDataWithRelations);
+			}
+			res.status(200).json({
+				result,
+			});
+		} else {
+			res.status(500).send('Match ya existe');
+		}
+	} catch (error) {
+		console.error('Error during registered:', error);
+		res.status(500).send('registered failed');
+	}
 };
 
 export const requestMatch = async (req, res, next) => {
-  console.log(req.body);
-  const { usuario_request, usuario_id } = req.body;
+	console.log(req.body);
+	const { usuario_request, usuario_id } = req.body;
 
-  try {
-    await pool.query(
-      "SELECT * FROM usuario_solicitudes WHERE solicitante_id = ? AND receptor_id = ?",
-      [usuario_request.id, usuario_id]
-    );
+	try {
+		await pool.query(
+			'SELECT * FROM usuario_solicitudes WHERE solicitante_id = ? AND receptor_id = ?',
+			[usuario_request.id, usuario_id],
+		);
 
-    if (true) {
-      const [result]: any = await pool.query(
-        "INSERT INTO usuario_solicitudes (solicitante_id,receptor_id,fecha_solicitud,estado) VALUES (?, ?, ?, ?)",
-        [usuario_request.id, usuario_id, new Date().toISOString(), "pendiente"]
-      );
+		if (true) {
+			const [result]: any = await pool.query(
+				'INSERT INTO usuario_solicitudes (solicitante_id,receptor_id,fecha_solicitud,estado) VALUES (?, ?, ?, ?)',
+				[usuario_request.id, usuario_id, new Date().toISOString(), 'pendiente'],
+			);
 
-      // io.emit('matchRequest', req.body);
+			// io.emit('matchRequest', req.body);
 
-      const datos = {
-        usuario_id: usuario_id,
-        title: "¡Nueva solicitud!",
-        message: `${usuario_request.nombre} te ha enviado una solicitud.`,
-        data: usuario_request,
-        time: new Date().toISOString(),
-        type: "match",
-        estado: "no leido",
-      };
-      req.body = datos;
-      next();
-    } else {
-      res.status(500).send("Solicitud ya enviada");
-    }
-  } catch (error) {
-    console.error("Error during registered:", error);
-    res.status(500).send("registered failed");
-  }
+			const datos = {
+				usuario_id: usuario_id,
+				title: '¡Nueva solicitud!',
+				message: `${usuario_request.nombre} te ha enviado una solicitud.`,
+				data: usuario_request,
+				time: new Date().toISOString(),
+				type: 'match',
+				estado: 'no leido',
+			};
+			req.body = datos;
+			next();
+		} else {
+			res.status(500).send('Solicitud ya enviada');
+		}
+	} catch (error) {
+		console.error('Error during registered:', error);
+		res.status(500).send('registered failed');
+	}
 };
 
 export const readPendingMatch = async (req, res) => {
-  try {
-    const token = req.headers["x-access-token"];
+	try {
+		const token = req.headers['x-access-token'];
 
-    const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
+		const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
 
-    const [results]: any = await pool.query(
-      "SELECT * FROM usuario_solicitudes WHERE receptor_id = ? AND estado = ?",
-      [decoded.id, "pendiente"]
-    );
-    console.log(results);
-    if (results.length === 0) {
-      return res.status(200).json([]);
-    } else {
-      const ids = results.map((c) => ` id = ${c.solicitante_id} `).join(" OR");
-      const [result]: any = await pool.query(
-        `SELECT * FROM usuario WHERE ${ids}`
-      );
-      res.status(200).json(result);
-    }
-  } catch (error) {
-    console.error("Error during registered:", error);
-    res.status(500).send("registered failed");
-  }
+		const [results]: any = await pool.query(
+			'SELECT * FROM usuario_solicitudes WHERE receptor_id = ? AND estado = ?',
+			[decoded.id, 'pendiente'],
+		);
+		console.log(results);
+		if (results.length === 0) {
+			return res.status(200).json([]);
+		} else {
+			const ids = results.map((c) => ` id = ${c.solicitante_id} `).join(' OR');
+			const [result]: any = await pool.query(
+				`SELECT * FROM usuario WHERE ${ids}`,
+			);
+			res.status(200).json(result);
+		}
+	} catch (error) {
+		console.error('Error during registered:', error);
+		res.status(500).send('registered failed');
+	}
 };
 
 export const readMyRequest = async (req, res) => {
-  try {
-    const token = req.headers["x-access-token"];
+	try {
+		const token = req.headers['x-access-token'];
 
-    const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
-    console.log("id:::", decoded.id);
+		const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
+		console.log('id:::', decoded.id);
 
-    const [results]: any = await pool.query(
-      "SELECT * FROM usuario_solicitudes WHERE solicitante_id = ? AND estado = ?",
-      [decoded.id, "pendiente"]
-    );
-    console.log(results);
-    if (results.length === 0) {
-      return res.status(200).json([]);
-    } else {
-      const ids = results.map((c) => ` id = ${c.receptor_id} `).join(" OR");
-      const [result]: any = await pool.query(
-        `SELECT * FROM usuario WHERE ${ids}`
-      );
-      res.status(200).json(result);
-    }
-  } catch (error) {
-    console.error("Error during registered:", error);
-    res.status(500).send("registered failed");
-  }
+		const [results]: any = await pool.query(
+			'SELECT * FROM usuario_solicitudes WHERE solicitante_id = ? AND estado = ?',
+			[decoded.id, 'pendiente'],
+		);
+		console.log(results);
+		if (results.length === 0) {
+			return res.status(200).json([]);
+		} else {
+			const ids = results.map((c) => ` id = ${c.receptor_id} `).join(' OR');
+			const [result]: any = await pool.query(
+				`SELECT * FROM usuario WHERE ${ids}`,
+			);
+			res.status(200).json(result);
+		}
+	} catch (error) {
+		console.error('Error during registered:', error);
+		res.status(500).send('registered failed');
+	}
 };
 
 export const readMatch = async (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-  try {
-    const [results]: any = await pool.query(
-      "SELECT * FROM usuario_contacto WHERE usuario_id = ?",
-      [id]
-    );
-    console.log(results);
-    if (results.length === 0) {
-      return res.status(200).json( [] );
-    } else {
-      const ids = results.map((c) => ` id = ${c.contacto_id} `).join(" OR");
-      const [result]: any = await pool.query(
-        `SELECT * FROM usuario WHERE ${ids}`
-      );
-      const usuarios: any[] = [];
-      for (const user of result) {
-		  const [lenguajesData]: any = await pool.query(
-			  "SELECT lenguaje FROM usuariolenguajes WHERE usuario_id = ?",
-			  [id]
-        );
+	const { id } = req.params;
+	console.log(id);
+	try {
+		const [results]: any = await pool.query(
+			'SELECT * FROM usuario_contacto WHERE usuario_id = ?',
+			[id],
+		);
+		console.log(results);
+		if (results.length === 0) {
+			return res.status(200).json([]);
+		} else {
+			const ids = results.map((c) => ` id = ${c.contacto_id} `).join(' OR');
+			const [result]: any = await pool.query(
+				`SELECT * FROM usuario WHERE ${ids}`,
+			);
+			const usuarios: any[] = [];
+			for (const user of result) {
+				const [lenguajesData]: any = await pool.query(
+					'SELECT lenguaje FROM usuariolenguajes WHERE usuario_id = ?',
+					[id],
+				);
 
-        const [areaExperienciaData]: any = await pool.query(
-          "SELECT experiencia FROM usuarioareaexperiencia WHERE usuario_id = ?",
-          [id]
-        );
+				const [areaExperienciaData]: any = await pool.query(
+					'SELECT experiencia FROM usuarioareaexperiencia WHERE usuario_id = ?',
+					[id],
+				);
 
-        const [temasInteresData]: any = await pool.query(
-          "SELECT interes FROM usuariotemasinteres WHERE usuario_id = ?",
-          [id]
-        );
+				const [temasInteresData]: any = await pool.query(
+					'SELECT interes FROM usuariotemasinteres WHERE usuario_id = ?',
+					[id],
+				);
 
-        const [tipoConexionData]: any = await pool.query(
-          "SELECT conexion FROM usuariotipoconexion WHERE usuario_id = ?",
-          [id]
-        );
+				const [tipoConexionData]: any = await pool.query(
+					'SELECT conexion FROM usuariotipoconexion WHERE usuario_id = ?',
+					[id],
+				);
 
-        const userDataWithRelations = {
-          ...user,
-          lenguajes: lenguajesData.map((row) => row.lenguaje),
-          areaExperiencia: areaExperienciaData.map((row) => row.experiencia),
-          temasInteres: temasInteresData.map((row) => row.interes),
-          tipoConexion: tipoConexionData.map((row) => row.conexion),
-        };
-        usuarios.push(userDataWithRelations);
-      }
-	  console.log("object", usuarios);
+				const userDataWithRelations = {
+					...user,
+					lenguajes: lenguajesData.map((row) => row.lenguaje),
+					areaExperiencia: areaExperienciaData.map((row) => row.experiencia),
+					temasInteres: temasInteresData.map((row) => row.interes),
+					tipoConexion: tipoConexionData.map((row) => row.conexion),
+				};
+				usuarios.push(userDataWithRelations);
+			}
+			console.log('object', usuarios);
 
-	  res.json(usuarios);
-
-    }
-  } catch (error) {
-    console.error("Error during registered:", error);
-    res.status(500).send("registered failed");
-  }
+			res.json(usuarios);
+		}
+	} catch (error) {
+		console.error('Error during registered:', error);
+		res.status(500).send('registered failed');
+	}
 };
 
 export const deleteRequestMatch = async (req, res) => {
-  const { id } = req.params;
-  const token = req.headers["x-access-token"];
-  try {
-    const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
-    await pool.query(
-      "DELETE FROM usuario_solicitudes WHERE solicitante_id = ? AND receptor_id = ?",
-      [decoded.id, id]
-    );
-    res.status(200).json({
-      result: "ok",
-    });
-  } catch (error) {
-    console.error("Error during registered:", error);
-    res.status(500).send("registered failed");
-  }
+	const { id } = req.params;
+	const token = req.headers['x-access-token'];
+	try {
+		const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
+		await pool.query(
+			'DELETE FROM usuario_solicitudes WHERE solicitante_id = ? AND receptor_id = ?',
+			[decoded.id, id],
+		);
+
+		const ioSocket = getSocketInstance();
+		const socketId = await getSocketId(id);
+
+		if (socketId) {
+			ioSocket.to(socketId).emit('deleteRequestMatch', { id: decoded.id });
+		}
+
+		res.status(200).json({
+			result: 'ok',
+		});
+	} catch (error) {
+		console.error('Error during registered:', error);
+		res.status(500).send('registered failed');
+	}
 };
 
 export const rejectPendingMatch = async (req, res) => {
-  const { id } = req.params;
-  const token = req.headers["x-access-token"];
-  try {
-    const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
-    await pool.query(
-      "DELETE FROM usuario_solicitudes WHERE solicitante_id = ? AND receptor_id = ?",
-      [id, decoded.id]
-    );
-    res.status(200).json({
-      result: "ok",
-    });
-  } catch (error) {
-    console.error("Error during registered:", error);
-    res.status(500).send("registered failed");
-  }
+	const { id } = req.params;
+	const token = req.headers['x-access-token'];
+	try {
+		const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
+		await pool.query(
+			'DELETE FROM usuario_solicitudes WHERE solicitante_id = ? AND receptor_id = ?',
+			[id, decoded.id],
+		);
+		const ioSocket = getSocketInstance();
+		const socketId = await getSocketId(id);
+
+		if (socketId) {
+			ioSocket.to(socketId).emit('rejectPendingMatch', { id: decoded.id });
+		}
+		res.status(200).json({
+			result: 'ok',
+		});
+	} catch (error) {
+		console.error('Error during registered:', error);
+		res.status(500).send('registered failed');
+	}
 };
 
 export const deleteMatch = async (req, res) => {
-  const { id } = req.params;
-  const token = req.headers["x-access-token"];
+	const { id } = req.params;
+	const token = req.headers['x-access-token'];
 
-  try {
-    const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
+	try {
+		const decoded: any = jwt.verify(token, configEnv.SECRET_KEY);
 
-    const [results]: any = await pool.query(
-      "SELECT * FROM usuario_contacto WHERE usuario_id = ?",
-      [decoded.id]
-    );
+		const [results]: any = await pool.query(
+			'SELECT * FROM usuario_contacto WHERE usuario_id = ?',
+			[decoded.id],
+		);
 
-    if (results.findIndex((c) => c.contacto_id == id) != -1) {
-      const [result]: any = await pool.query(
-        "DELETE FROM usuario_contacto WHERE usuario_id = ? AND contacto_id = ?",
-        [decoded.id, id]
-      );
-      pool.query(
-        "DELETE FROM usuario_contacto WHERE usuario_id = ? AND contacto_id = ?",
-        [id, decoded.id]
-      );
-      res.status(200).json({
-        result,
-      });
-    } else {
-      res.status(500).send("User does not exist");
-    }
-  } catch (error) {
-    console.error("Error during registered:", error);
-    res.status(500).send("registered failed");
-  }
+		if (results.findIndex((c) => c.contacto_id == id) != -1) {
+			const [result]: any = await pool.query(
+				'DELETE FROM usuario_contacto WHERE usuario_id = ? AND contacto_id = ?',
+				[decoded.id, id],
+			);
+			pool.query(
+				'DELETE FROM usuario_contacto WHERE usuario_id = ? AND contacto_id = ?',
+				[id, decoded.id],
+			);
+			const ioSocket = getSocketInstance();
+			const socketId = await getSocketId(id);
+			if (socketId) {
+				ioSocket.to(socketId).emit('deleteMatch', { id: decoded.id });
+			}
+			res.status(200).json({
+				result,
+			});
+		} else {
+			res.status(500).send('User does not exist');
+		}
+	} catch (error) {
+		console.error('Error during registered:', error);
+		res.status(500).send('registered failed');
+	}
 };
